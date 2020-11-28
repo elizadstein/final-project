631836b712d8594ef359d36f3dba1ff33498284c
elizadstein <elizadstein@gmail.com>
2020-11-27 21:29:30 -0500

    changed paths in testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,8 +9,8 @@ tinytex::install_tinytex()
 
 
 
-knitr::knit('analysis/pd_analysis.Rmd', tangle=TRUE)
+knitr::knit('../analysis/pd_analysis.Rmd', tangle=TRUE)
 source('pd_analysis.R')
-testthat::test_file('tests/testthat/test-1.R')
-testthat::test_file('tests/testthat/test-2.R')
+testthat::test_file('../tests/testthat/test-1.R')
+testthat::test_file('../tests/testthat/test-2.R')
 system('-rm pd_analysis.R')
